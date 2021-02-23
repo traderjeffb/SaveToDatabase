3404999471388993f04140771c852504078b7771
traderjeffb <traderjeffb@gmail.com>
2021-02-23 09:21:11 -0500

    added ability to list

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+<form action="create.php" method="post">
+
+  <label for="address1">Address :</label>
+  <input type="text" id="address1" name="address1"><br><br>
+
+  <label for="address2">Address :</label>
+  <input type="text" id="address2" name="address2"><br><br>
+
+  <label for="city">City:</label>
+  <input type="text" id="city" name="city"><br><br>
+
+  <label for="state">State:</label>
+  <input type="text" id="state" name="state"><br><br>
+
+  <label for="zip5">Zipcode:</label>
+  <input type="text" id="zip5" name="zip5"><br><br>
+
+  <label for="zip4">Zipcode extention:</label>
+  <input type="text" id="zip4" name="zip4"><br><br>
+  <input type="submit" value="submit">
+</form>
+<div>
+
+<form action="list.php" method="post">
+  <input type="submit" value="List"name="List">
+</form>
